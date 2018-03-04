349bc46069f2c673b44620d2aadf09090716373b
Susan Marek <smarek@chaskamn.com>
2018-03-04 11:41:13 -0600

    working burger app

--- controllers/burgers_controller.js
+++ controllers/burgers_controller.js
@@ -28,22 +28,22 @@ router.post("/api/burgers", function(req, res) {
   });
 });
 
-// router.put("/api/burgers/:id", function(req, res) {
-//   var condition = "id = " + req.params.id;
+router.put("/api/burgers/:id", function(req, res) {
+  var condition = "id = " + req.params.id;
 
-//   console.log("condition", condition);
+  console.log("condition", condition);
 
-//   burgerAction.updateOne({
-//     devoured: req.body.devoured
-//   }, condition, function(result) {
-//     if (result.changedRows == 0) {
-//       // If no rows were changed, then the ID must not exist, so 404
-//       return res.status(404).end();
-//     } else {
-//       res.status(200).end();
-//     }
-//   });
-// });
+  burgerAction.updateOne({
+    devoured: req.body.devoured
+  }, condition, function(result) {
+    if (result.changedRows == 0) {
+      // If no rows were changed, then the ID must not exist, so 404
+      return res.status(404).end();
+    } else {
+      res.status(200).end();
+    }
+  });
+});
 
 // router.delete("/api/burgers/:id", function(req, res) {
 //   var condition = "id = " + req.params.id;
--- models/burger.js
+++ models/burger.js
@@ -13,11 +13,11 @@ var burgerAction = {
       cb(res);
     });
   },
-  // updateOne: function(objColVals, condition, cb) {
-  //   orm.updateOne("burgers", objColVals, condition, function(res) {
-  //     cb(res);
-  //   });
-  // },
+  updateOne: function(objColVals, condition, cb) {
+    orm.updateOne("burgers", objColVals, condition, function(res) {
+      cb(res);
+    });
+  },
   // delete: function(condition, cb) {
   //   orm.delete("burgers", condition, function(res) {
   //     cb(res);
